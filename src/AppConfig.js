d562e6da3079015d1f96db19ab3cbe7b1c041cb3
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-09-11 09:25:44 +0530

    Add app data and config

---
+++ src/AppConfig.js
@@ -0,0 +1,8 @@
+export const taskBoardConfig = {
+    // Url for resource avatar images
+    resourceImagePath: "users/",
+    // default columns
+    columns: ["todo", "doing", "done"],
+    // Field used to pair a task to a column
+    columnField: "status",
+  };
